0c8c13921862f49796685b222f295adec23d22c7
edlu2100 <edwinlundback@gmail.com>
2026-02-28 17:36:25 +0100

    publish

---
+++ src/config/emailjs.ts
@@ -0,0 +1,3 @@
+export const SERVICE_ID  = import.meta.env.VITE_EMAILJS_SERVICE_ID as string
+export const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string
+export const PUBLIC_KEY  = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string
